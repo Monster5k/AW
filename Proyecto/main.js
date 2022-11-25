31277775b904dc0f41cc6865a252e1498e934049
Monster5k <linascijunskisdodonova@gmail.com>
2022-11-25 13:54:12 +0100

    arreglo de javascrpt

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -1,12 +1,17 @@
 const main_img = document.querySelector('.main_img')
 const thumbnails = document.querySelectorAll('.thumbnail')
 
+function cambiarImagen(imagen){
+    var i = document.getElementById("mainImg");
+    i.src = imagen;
+}
+
 
 thumbnails.forEach(thumb => {
     thumb.addEventListener('click', function(){
         const active = document.querySelector('.active')
         active.classList.remove('active')
-        thumb.classList.add('active')
+        this.classList.add('active')
         main_img.src = thumb.src
     })
 })
